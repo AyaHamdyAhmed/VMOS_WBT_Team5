ee7870d6c53f7b98337b67e1e51ae5c05a805ebb
AyaMohamed <amm.e2011@yahoo.com>
2019-04-03 17:54:36 +0200

    soft assertion applied

--- src/vehicle/test/StateTest.java
+++ src/vehicle/test/StateTest.java
@@ -17,10 +17,6 @@ class StateTest {
 		Temp.r1 = 1;
 		Temp.r2 = 2;
 		boolean ret = stateObj.bValidateFailure();	
-		assertEquals(1, Temp.validateCall1);
-		assertEquals(1, Temp.validateCall2);
-		assertTrue(ret);
-		
 		assertAll(
 				
 				() ->	assertEquals(1, Temp.validateCall1),
