69f4888a0558aec0b129a5c8774b160646e9051b
Asmaa Hamdy <asmaahamdy250@gmail.com>
2019-04-03 12:46:25 +0200

    vidMain function is done
    vidMain function is done

--- src/vehicle/app/Main.java
+++ src/vehicle/app/Main.java
@@ -1,7 +1,13 @@
 package vehicle.app;
 
+import vehicle.hal.LCD;
+import vehicle.hal.Motor;
+
 public class Main {
 	public void vidMain() {
-		
+		State state = new State();
+		state.vidGoToNextState();
+		LCD.vidOutput(Global.iState);
+		Motor.vidOutput(Global.iState);	
 	}
 }
