6dcd966280442d845b70ceeb5ddf0c8ed3f1611b
sara-sayed <33573318+sara-sayed@users.noreply.github.com>
2019-04-03 13:27:30 +0200

    modify StateTest

--- src/vehicle/app/State.java
+++ src/vehicle/app/State.java
@@ -1,23 +1,17 @@
 package vehicle.app;
 
 public class State {
-<<<<<<< HEAD
-	
-	public static int validate_call_times=0;
-	public static int validate_call_number =0;
+
+	public static int validate_call_times = 0;
+	public static int validate_call_number = 0;
 	
 	public Boolean bValidateFailure()
 	{
 		validate_call_times++ ;
 		validate_call_number=1;
 		return true;
-=======
-
-	public Boolean bValidateFailure() {
-		return false;
->>>>>>> 870dbbd696bbdc574c0fe49bc6967c0b0fe52d68
 	}
-	
+
 	public Boolean bValidateLowPerformance()
 	{
 		validate_call_times++ ;
@@ -31,17 +25,12 @@ public class State {
 		validate_call_number=3;
 		return false;
 	}
-<<<<<<< HEAD
 
-	public void vidGoToNextState() {
- 
-		
-		
-	}
-=======
+	public void vidGoToNextState()
+	{
 	
-	public void vidGoToNextState() {
-		
 	}
 }
->>>>>>> 870dbbd696bbdc574c0fe49bc6967c0b0fe52d68
+ 
+		
+	
--- src/vehicle/app/StateTest.java
+++ src/vehicle/app/StateTest.java
@@ -8,49 +8,42 @@ import org.junit.jupiter.api.Test;
 
 class StateTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	State stateObj;
+	
 	@Test
-	void test () {
-/*	void testvidGoToNextState_T_F_F() {
-		State testobj = new State();
-		testobj.vidGoToNextState();
+	void testVidGoToNextState_T_F_F() {
+		stateObj = new State();
+		stateObj.vidGoToNextState();
 		assertEquals(1,State.validate_call_number);
 		assertEquals(1,State.validate_call_times);
 		assertEquals(4,Global.iState);
 	
 	}
 	
-	void testvidGoToNextState_F_T_F() {
-		State testobj = new State();
-		testobj.vidGoToNextState();
+	void testVidGoToNextState_F_T_F() {
+		stateObj = new State();
+		stateObj.vidGoToNextState();
 		assertEquals(2,State.validate_call_number);
 		assertEquals(1,State.validate_call_times);
 		assertEquals(1,Global.iState);
 	
 	}
 
-	void testvidGoToNextState_F_F_T() {
-		State testobj = new State();
-		testobj.vidGoToNextState();
+	void testVidGoToNextState_F_F_T() {
+		stateObj = new State();
+		stateObj.vidGoToNextState();
 		assertEquals(3,State.validate_call_number);
 		assertEquals(1,State.validate_call_times);
 		assertEquals(2,Global.iState);
 	
 	}
-	void testvidGoToNextState_F_F_F() {
-		State testobj = new State();
-		testobj.vidGoToNextState();
+	void testVidGoToNextState_F_F_F() {
+		stateObj = new State();
+		stateObj.vidGoToNextState();
 		assertEquals(0,State.validate_call_number);
 		assertEquals(0,State.validate_call_times);
 		assertEquals(3,Global.iState);
 	
-	}*/
 	}
+	
 }
