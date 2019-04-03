8a013684aee57ac592a7550c2ff127d6cfdfd50a
AyaMohamed <amm.e2011@yahoo.com>
2019-04-03 13:52:44 +0200

    Temp class Added ya salsabeeeeel

--- src/vehicle/hal/Temp.java
+++ src/vehicle/hal/Temp.java
@@ -1,5 +1,23 @@
 package vehicle.hal;
 
 public class Temp {
+	
+	public static int r1;
+	public static int r2;
+	public static int validateCall1=0;
+	public static int validateCall2=0;
+	public static int iReadT1()
+	{
+		validateCall1++;
+		return r1;
+		
+	}
+	
+	public static int iReadT2()
+	{
+		validateCall2++;
+		return r2;
+		
+	}
 
 }
