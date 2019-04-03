4ca9e6a9b2d67897cb0f9b8c8e3d8fd11e86b84e
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-04-03 14:00:58 +0200

    change varible sequence name

--- src/vehicle/hal/LCD.java
+++ src/vehicle/hal/LCD.java
@@ -3,13 +3,13 @@ package vehicle.hal;
 public class LCD {
 	
 	public static int lcdcall;
-	public static int lcdCallNumber = 0;
+	public static int lcdSequenceNumber = 0;
 	public static int lcdState;
 	public static void vidOutput(int state) {
 		
 		lcdcall++;
 		lcdState=state;
-		lcdCallNumber=1;
+		lcdSequenceNumber=1;
 	}
 
 }
