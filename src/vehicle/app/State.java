ea0c2d697bf5989411ac16552a34e0be28288eca
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-04-03 15:09:46 +0200

    conflict in State class resolved

--- src/vehicle/app/State.java
+++ src/vehicle/app/State.java
@@ -1,20 +1,14 @@
 package vehicle.app;
 
 public class State {
-<<<<<<< HEAD
 	
 	public static int validate_call_times=0;
 	public static int validate_call_bValidateFailure =0;
 	public static int validate_call_bValidateLowPerformance =0;
 	public static int validate_call_bValidateHighPerformance =0;
 	public static int validate_call_bValidateGoToNextState=0;
-
-=======
-
-	public static int validate_call_times = 0;
 	public static int validate_call_number = 0;
 	
->>>>>>> 2feadc03def3832131a418e6e5266ffd6757668d
 	public Boolean bValidateFailure()
 	{
 		validate_call_times++ ;
@@ -37,27 +31,9 @@ public class State {
 		return false;
 	}
 
-<<<<<<< HEAD
 	public void vidGoToNextState() {
  
 	validate_call_bValidateGoToNextState++;
 		
 	}
-
-	
-	
-   
-
-	
-
 }
-=======
-	public void vidGoToNextState()
-	{
-	validate_call_bValidateGoToNextState++;
-	}
-}
- 
-		
-	
->>>>>>> 2feadc03def3832131a418e6e5266ffd6757668d
