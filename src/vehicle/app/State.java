38ca92f3502e84bd7c9a3214b1420322480c07aa
sara-sayed <33573318+sara-sayed@users.noreply.github.com>
2019-04-03 11:22:37 +0200

    Add stubs for test vidGoToNextState method in class State

--- src/vehicle/app/State.java
+++ src/vehicle/app/State.java
@@ -1,5 +1,16 @@
 package vehicle.app;
 
 public class State {
-
+	
+	public Boolean bValidateFailure() {
+		return false;
+	}
+	
+	public Boolean bValidateLowPerformance() {
+		return false;
+	}
+	
+	public Boolean bValidateHighPerformance() {
+		return false;
+	}
 }
