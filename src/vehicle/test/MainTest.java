092fb23bdb4b8f61ff183b0b5ab2df8463be6c69
Asmaa Hamdy <asmaahamdy250@gmail.com>
2019-04-03 15:00:37 +0200

    Assertions are added to MainTest class

--- src/vehicle/test/MainTest.java
+++ src/vehicle/test/MainTest.java
@@ -5,18 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import vehicle.app.State;
+import vehicle.hal.LCD;
+import vehicle.hal.Motor;
 
 class MainTest {
 	State state;
+
 	@Test
 	void validateMaintest() {
 		state = new State();
-		assertAll(
-				()-> assertEquals(1,state.validate_call_bValidateGoToNextState),
-				
-				
-				);
-	
+		assertAll(() -> assertEquals(1, state.validate_call_bValidateGoToNextState),
+				() -> assertEquals(1, Motor.motorCall),
+				() -> assertEquals(1, LCD.lcdcall),
+				() -> assertEquals(1, LCD.lcdSequenceNumber),
+				() -> assertEquals(2, LCD.lcdSequenceNumber),
+				()-> assertEquals(0,Motor.motorState),
+				()-> assertEquals(0,LCD.lcdState)
+
+		);
+
 	}
 
 }
