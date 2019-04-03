4e86c1fc8c528d07a4c582a9774335a935cd0c12
Asmaa Hamdy <asmaahamdy250@gmail.com>
2019-04-03 17:42:55 +0200

    The test runner class is updated

--- src/unittest/TestRunner.java
+++ src/unittest/TestRunner.java
@@ -7,8 +7,8 @@ public class TestRunner {
 
 	public static void main(String args[]) {
 		Result result = new JUnitCore().run(AllTests.class);
-		System.out.println("The number of test cases passed in MainTest class " + result.getRunCount());
-		System.out.println("The number of test cases failed in MainTest class " + result.getRunCount());
+		System.out.println("The number of test cases passed " + result.getRunCount());
+		System.out.println("The number of test cases failed " + result.getRunCount());
 
 	}
 }
