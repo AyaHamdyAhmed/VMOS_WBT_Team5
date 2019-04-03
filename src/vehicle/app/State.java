ed744070e8f3bc12143fb729150ae67d711a133b
Khadija Moustafa <khadija.mostafa93@gmail.com>
2019-04-03 12:41:02 -0700

    add TestState class and modify stubs in state class

--- src/vehicle/app/State.java
+++ src/vehicle/app/State.java
@@ -2,15 +2,32 @@ package vehicle.app;
 
 public class State {
 	
-	public Boolean bValidateFailure() {
+	public static int validate_call_times=0;
+	public static int validate_call_number =0;
+	
+	public Boolean bValidateFailure()
+	{
+		validate_call_times++ ;
+		validate_call_number=1;
+		return true;
+	}
+	
+	public Boolean bValidateLowPerformance()
+	{
+		validate_call_times++ ;
+		validate_call_number=2;
 		return false;
 	}
 	
-	public Boolean bValidateLowPerformance() {
+	public Boolean bValidateHighPerformance()
+	{
+		validate_call_times++ ;
+		validate_call_number=3;
 		return false;
 	}
-	
-	public Boolean bValidateHighPerformance() {
-		return false;
+
+	public void vidGoToNextState() {
+ 
+		
+		
 	}
-}
