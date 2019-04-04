613413d48158d4c6ae99522d6a0d93ddd606fd98
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2019-04-04 09:11:11 +0200

    call Method VidMain

--- src/vehicle/test/MainTest.java
+++ src/vehicle/test/MainTest.java
@@ -4,19 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import vehicle.app.Main;
 import vehicle.app.State;
 import vehicle.hal.LCD;
 import vehicle.hal.Motor;
 
 
 public class MainTest {
-
+Main main;
 
 	@Test
 	public void validateMaintest() {
 
-		
-
+	
+main = new Main();
+main.vidMain();
 		// soft assert
 		assertAll(
 				() -> assertEquals(1, State.validate_call_bValidateGoToNextState),
